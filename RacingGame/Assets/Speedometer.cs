3037d20952eb2e7bc79d6cdae40bd496c930fb9c
DJTechZombie <46727082+DJTechZombie@users.noreply.github.com>
2019-04-01 23:19:04 -0500

    Racing Game Added

---
+++ RacingGame/Assets/Speedometer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.Vehicles.Car;
+
+public class Speedometer : MonoBehaviour
+{
+    public CarController carController;
+    public Text speedText;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        speedText.text = carController.CurrentSpeed.ToString("000");
+    }
+}
