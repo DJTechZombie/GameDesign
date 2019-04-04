717d282854ddb0dfe480281259afddcf99a56915
DJTechZombie <46727082+DJTechZombie@users.noreply.github.com>
2019-04-04 00:47:49 -0500

    Adding Leader tracking

---
+++ RacingGame/Assets/LeadTracker.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeadTracker : MonoBehaviour
+{
+    public GameObject[] waypoints;
+    public GameObject waypointTracker;
+    public string leadCar;
+
+    public int currentWaypoint;
+
+    // Update is called once per frame
+    void Update()
+    {
+        waypointTracker.transform.position = waypoints[currentWaypoint].transform.position;
+        waypointTracker.transform.rotation = waypoints[currentWaypoint].transform.rotation;
+    }
+
+    private IEnumerator OnTriggerEnter(Collider other)
+    {
+        leadCar = other.gameObject.name;
+            this.GetComponent<BoxCollider>().enabled = false;
+            currentWaypoint++;
+            if (currentWaypoint >= waypoints.Length)
+            {
+                Debug.Log("Reset Waypoints");
+                currentWaypoint = 0;
+            }
+            else
+            {
+            Debug.Log(other.transform.root.tag + " leading");
+            }
+            yield return new WaitForSeconds(1);
+            this.GetComponent<BoxCollider>().enabled = true;
+    }
+}
