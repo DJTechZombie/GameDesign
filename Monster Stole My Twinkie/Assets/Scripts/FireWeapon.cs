642470bb6a77b4b4b13717b12df260d98e4822c5
DJTechZombie <46727082+DJTechZombie@users.noreply.github.com>
2019-04-30 20:42:09 -0500

    ModelsAdded

---
+++ Monster Stole My Twinkie/Assets/Scripts/FireWeapon.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireWeapon : MonoBehaviour
+{
+
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    public float fireRate = 2f;
+
+    private float nextFire;
+    
+
+    public void ShootWeapon()
+    {
+        if (Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            FireRay();
+            GameObject spawnedBullet = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation); ;
+            Destroy(spawnedBullet, 3f);
+
+        }
+    }
+
+    private void FireRay()
+    {
+        Debug.DrawRay(bulletSpawn.transform.position, bulletSpawn.transform.forward *100, Color.red, 1f);
+    }
+}
