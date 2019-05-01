bf8ab058c95ad93dcadf0f738a9e3e6c9d4669a5
DJTechZombie <46727082+DJTechZombie@users.noreply.github.com>
2019-05-01 01:07:19 -0500

    Added Radar

---
+++ Monster Stole My Twinkie/Assets/Scripts/CollectNeighbor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectNeighbor : MonoBehaviour
+{
+   
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Debug.Log("You Saved " + gameObject.name);
+            Destroy(gameObject, .5f);
+        }
+    }
+}
---
+++ Monster Stole My Twinkie/Assets/Scripts/CreateRadarObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateRadarObject : MonoBehaviour
+{
+    public Image image;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Radar.RegisterRadarObject(this.gameObject, image);
+    }
+
+    private void OnDestroy()
+    {
+        Radar.RemoveRadarObject(this.gameObject);
+    }
+
+}
